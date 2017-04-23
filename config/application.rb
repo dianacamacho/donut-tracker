b359b34d66afae75e754935415ec519ca7302014
dianacamacho <dcam1901@yahoo.com>
2017-04-22 22:20:24 -0500

    app time zone and db time set to local cst

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module DonutTracker
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = "Central Time (US & Canada)"
+    config.active_record.default_timezone = :local
     config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
