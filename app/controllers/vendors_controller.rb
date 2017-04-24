e870800fc20880f84e3f73b741fcb56533119f8f
dianacamacho <dcam1901@yahoo.com>
2017-04-24 00:28:40 -0500

    send a text after vendor create

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -15,6 +15,7 @@ class VendorsController < ApplicationController
     @vendor = Vendor.new(vendor_params)
 
     if @vendor.save
+      trigger_sms_alerts(@vendor)
       redirect_to vendors_path
     else 
       render :new
@@ -45,5 +46,27 @@ class VendorsController < ApplicationController
 
     def vendor_params
       params.require(:vendor).permit(:name, :twitter, :hashtag, :opening_time, :address, :email, :image)
+    end
+
+    def boot_twilio
+      @client =  Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+    end
+
+    def trigger_sms_alerts(vendor)
+      @message = "Hey there! New vendor: #{vendor.name}"
+      send_message(ENV["PHONE_NUMBER"], @message)
+    end
+
+    def send_message(phone_number, alert_message)
+      @twilio_number = ENV['TWILIO_NUMBER']
+      boot_twilio      
+      message = @client.account.messages.create(
+        :from => @twilio_number,
+        :to => phone_number,
+        :body => alert_message,
+        # US phone numbers can make use of an image as well.
+        # :media_url => image_url 
+      )
+      puts message.to
     end
 end
